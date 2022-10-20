6f4c220b771502ac22e41491293f6afa854bbba6
paulovitor <pvitor7@hotmail.com>
2022-10-20 16:29:58 -0300

    feat: conclued header and menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import GlobalStyle from './styles/global';
 import React from 'react';
+import { Header } from './components/header';
 
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Header/>
     </div>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,10 +2,7 @@ import { HeaderStyled, MenuStyled } from "./style";
 import MotorShop from "../../img/Motors-shop.svg";
 import Menu from "../../img/Menu-Header.svg";
 import CloseMenu from "../../img/Close-Menu.svg";
-
-
 import { useState } from "react";
-
 
 export const Header = () => {
 
@@ -18,41 +15,37 @@ export const Header = () => {
 
                 <nav className="nav-menu">
                     {!openMenu ?
-                        <button onClick={() => setOpenMenu(true)}>
-                            <img src={Menu} alt="" />
-
-                        </button>
+                        <button onClick={() => setOpenMenu(true)}> <img src={Menu} alt="" /> </button>
                         :
-                        <button
-                        
-                            onClick={() => setOpenMenu(false)}
-                        >
-                            <img src={CloseMenu} alt="" />
-                        </button>
+                        <button onClick={() => setOpenMenu(false)}> <img src={CloseMenu} alt="" /> </button>
                     }
+                </nav>
+
+
+                <nav className="nav--menu-desktop">
+                    <button className="nav--menu-desktop-button">Carros</button>
+                    <button className="nav--menu-desktop-button">Motos</button>
+                    <button className="nav--menu-desktop-button">Leilão</button>
+
+                    <div className="nav--login-register-desktop">
+                    <div id="div-line"></div>
+                        <h4 className="nav--menu-desktop-h4">Fazer Login</h4>
+                        <button className="nav--menu-desktop-button-register">Cadastrar</button>
+                    </div>
                 </nav>
             </HeaderStyled>
 
             {openMenu &&
                 <MenuStyled>
                     <nav className="nav--menu-mobile">
-                        <button className="nav--menu-mobile-button"
-                        >Carros</button>
-                        <button
-                        className="nav--menu-mobile-button"
-                        >Motos</button>
-                        <button
-                        className="nav--menu-mobile-button"
-                        >Leilão</button>
+                        <button className="nav--menu-mobile-button">Carros</button>
+                        <button className="nav--menu-mobile-button">Motos</button>
+                        <button className="nav--menu-mobile-button">Leilão</button>
                     </nav>
-                    <div>
-                        <h4>Fazer Login</h4>
-
-                        <button>
-                            Cadastrar
-                        </button>
+                    <div className="nav--login-register-mobile">
+                        <h4 className="nav--menu-mobile-h4">Fazer Login</h4>
+                        <button className="nav--menu-mobile-button">Cadastrar</button>
                     </div>
-
                 </MenuStyled>
             }
         </>
