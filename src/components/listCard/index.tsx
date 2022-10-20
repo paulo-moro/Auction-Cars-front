f6d8efd0d63a54527e1002ebc8cb1be8139f7b4f
elefantinhos2 <douglasramires01@gmail.com>
2022-10-20 05:08:07 -0200

    fix: add condictional in listCard

--- src/components/listCard/index.tsx
+++ src/components/listCard/index.tsx
@@ -1,10 +1,28 @@
 import { useState } from "react";
+import Card from "../card";
+
+
 
 const ListCard = () => {
 
+    const [listVehicle, setlistVehicle] = useState([
+        {
+            id: "1",
+            name: "Douglas Ramires",
+            img: 'https://th.bing.com/th/id/OIP.UQeKbZTigv91K96LGwGAZgHaE8?pid=ImgDet&rs=1',
+            title: "Product title stays here - max 1 line",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...",
+            KM: "0 KM",
+            year: "2019",
+            price: "R$ 00.000,00"
+        }
+    ]);
+
   return (
     <ul>
-
+      {listVehicle && listVehicle.map((vehicle) => (
+        <Card  key={vehicle.id} vehicles={vehicle} />
+      ))}
     </ul>
   );
 };
