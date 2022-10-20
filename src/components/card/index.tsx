57ea373ee3730220e7c9c7401da2beea62384f04
elefantinhos2 <douglasramires01@gmail.com>
2022-10-20 04:56:09 -0200

    feat: create example for listCard and function Card

---
+++ src/components/card/index.tsx
@@ -0,0 +1,12 @@
+import { VehiclesProps } from "../../interface"
+
+
+const Card = ({vehicles}: VehiclesProps) => {
+
+    return (
+        <li>
+        </li>
+    );
+};
+  
+export default Card;
