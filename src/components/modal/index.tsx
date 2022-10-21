03b3cbd2d2c5ac3b9fc27a3134ddfcffde6a786e
elefantinhos2 <douglasramires01@gmail.com>
2022-10-21 08:37:25 -0200

    feat: create function modal and structure

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,12 +1,42 @@
-
+import { useState } from "react";
 
 function Modal() {
+
+    const [inOn, setInOn]   = useState(false);
+    const [modal, setModal] = useState("containerModal hidden");
+
+
+    const showModal = () => {
+        if( inOn === false ) {
+            setInOn(true);
+            return setModal("containerModal")
+        } else {
+            setInOn(false);
+            return setModal("containerModal hidden")
+        }
+    }
+
+    const removeModal = () => {
+        setInOn(false);
+        return setModal("containerModal hidden")
+    }
 
 
     
     return (
         <section>
-           
+            <section className={modal}>
+                <div className="modal">
+                    <div className="modalHeader">
+                        <h1>Header Title</h1>
+                        <button className="removedModal" onClick={() => removeModal()}>x</button>
+                    </div>
+                    <div className="modalBody">
+                        <p>Estrutute o corpo do seu modal</p>
+                    </div>
+                </div>
+            </section>
+            <button onClick={() => showModal()}>+</button>
         </section>
     )
 }
